56f3da26747b92d66f51b05d446f51969445bb5b
Yazan Jumaa <ys2110319@qu.edu.qa>
2024-08-08 19:08:54 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,10 @@
-from sqlalchemy import create_engine, text 
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load the environment variables from the .env file
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine("postgresql+psycopg2://postgres.mhumhvlaihdfgegupqeu:yazanYAZAN2003@aws-0-ap-south-1.pooler.supabase.com:6543/postgres")
 
