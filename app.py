2fbbfc8a0d5d7943ccc4c8c2defb706f5d2bbd80
Yazan Jumaa <ys2110319@qu.edu.qa>
2024-08-08 22:06:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ def list_jobs():
 @app.route('/job/<id>')
 def show_job(id):
     job = load_job_from_db(id)
+    if not job:
+        return "Not Fount", 404
     return render_template('job_page.html', job = job)
 
  
