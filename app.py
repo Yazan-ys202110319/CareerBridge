be02bb2afb9c9bf302fac19408aab0ed0f2fc4ae
Yazan Jumaa <ys2110319@qu.edu.qa>
2024-08-08 20:53:02 +0300

    dynamic pages for the jobs

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_form_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    jobs = load_jobs_form_db()
+    jobs = load_jobs_from_db()
     return render_template('home.html', jobs = jobs)
+
 
 @app.route("/api/jobs")
 def list_jobs():
-    jobs = load_jobs_form_db()
+    jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+
+@app.route('/job/<id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
--- database.py
+++ database.py
@@ -9,10 +9,22 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL) # Load the database to the engine.
 
 
-def load_jobs_form_db():
+def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("select * from jobs"))
         jobs = []
         for row in result.all(): # jobs is a list
             jobs.append(row._asdict())
         return jobs
+    
+def load_job_from_db(id):
+    with engine.connect as conn:
+        result = conn.execute(
+            text('SELECT * FROM jobs WHERE id = :val'),
+            val = id
+        )
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0].__asdict()
