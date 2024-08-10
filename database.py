7b909cb674df2a7344f955efbae516863f886c19
Yazan Jumaa <ys2110319@qu.edu.qa>
2024-08-10 17:54:54 +0300

    added comments

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Load the environment variables from the .env file
+load_dotenv()  # Load the environment variables from the .env file.
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -10,7 +10,7 @@ engine = create_engine(DATABASE_URL) # Load the database to the engine.
 
 
 def load_jobs_from_db():
-    with engine.connect() as conn:
+    with engine.connect() as conn: # with ensures that the connection is automatically closed when the block is exited.
         result = conn.execute(text("select * from jobs"))
         jobs = []
         for row in result.all(): # jobs is a list
@@ -29,16 +29,17 @@ def load_job_from_db(id):
            return rows[0]._asdict()
         
 
-def add_application_to_db(job_id, data):
+def add_application_to_db(job_id, data): # data here is the application, and job means which job type the user want to apply 
     with engine.connect() as conn:
-        query = text(
+        query = text( # query create SQL query string for sqlalchemy.
+            # The place holders (:job_id, :full_name) to avoid SQL injuction.
             f"INSERT INTO applications(job_id, full_name, email, linkedin_url, education, work_experience, resume) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume)"
         )
 
-        conn.execute(
+        conn.execute( # execute() runs the SQL query.
             query,
             {
-                "job_id": job_id,
+                "job_id": job_id, # because there is only 1 value in the job_id which is the job id there is no need to specify the index like the application.
                 "full_name": data["full_name"],
                 "email": data["email"],
                 "linkedin_url": data["linkedin_url"],
@@ -49,26 +50,3 @@ def add_application_to_db(job_id, data):
         )
         conn.commit()
         
-# def add_application_to_db(job_id, data):
-
-#     with engine.connect() as conn:
-#         query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume)")
-#         conn.execute(query,  job_id = data['job_id'], full_name = data['full_name'],
-#                       email = data['email'],
-#                       linkedin_url = data['linkedin_url'],
-#                       education = data['education'],
-#                       work_experience = data['work_experience'],
-#                       resume = data['resume'])
-        
-#         conn.commit()
-        
-        # print(f"Inserted {result.rowcount} row(s) into the database.")
-
-        
-
-        # 'job_id' : job_id, 'full_name' : data['full_name'],
-        #              'email' : data['email'],
-        #              'linkedin_url' : data['linkedin_url'],
-        #              'education' : data['education'],
-        #              'work_experience' : data['work_experience'],
-        #              'resume' : data['resume']})
